1100fd89b1d06bb6a423bdacdec0b360b3013e5c
inyoungAum <inyoung.aum@ridi.com>
2018-11-18 17:54:05 +0900

    애기가 안나오면 리스트에서 빼도록 변경

--- api/child_list.php
+++ api/child_list.php
@@ -6,7 +6,7 @@
     $driver_id = $_GET["driver_id"]; 
 
     global $connect;
-    $sql    = 'SELECT c.idx, c.name, c.beacon_id, p.lat, p.lng FROM driver AS d, child AS c, parent AS p WHERE d.idx = '.$driver_id.' AND p.driver_idx = d.idx AND p.idx = c.parent_idx';
+    $sql    = 'SELECT c.idx, c.name, c.beacon_id, p.lat, p.lng FROM driver AS d, child AS c, parent AS p WHERE d.idx = '.$driver_id.' AND p.driver_idx = d.idx AND p.idx = c.parent_idx AND p.absent = 0';
     $result = mysql_query($sql, $connect);
 
     if (!$result) {
